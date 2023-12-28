73460e0d32cd78feebded4f00d64faea4a3e1122
vaibhavawasarkar <vs.scala.619@gmail.com>
2023-12-28 17:48:42 +0530

    #2

---
+++ BulkyApp/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+// First add the folder named 'Category' and add view in folder which is by default index.cshtml
+// after adding view add the controller in controllers folder , add controller of same name as of view
+// like for 'category' view add categorycontroller.cs controller, there must be controller name at the last
---
+++ BulkyApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        // Tools -> Nuget Package Manager -> Package Manager Consol
+        // In Console Type -> add-migration AddCategoryTableToDb
+        // It Will automaticaly create Migrations Folder which consists time stamps
+        // In Console Type -> update-database
+        // It will create database
+
+        
+    }
+}
